ea0f1f71568aadfdf79742d8d0b27c5d256fe75e
Chago 45 <svenegas24@yahoo.com>
2021-10-07 14:44:15 -0700

    beginning javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robots name?");
+
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robbot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
